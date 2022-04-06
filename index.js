95d70cff6ee31fc88bd30056138017a7d462369e
userssb <sreeramoju.sreekanthbabu@gmail.com>
2022-04-06 14:22:49 +0000

    committing congrats html page using react js

---
+++ index.js
@@ -0,0 +1,20 @@
+const element = (
+  <div class="bg-cont1">
+    <h1>Congratulations</h1>
+    <div class="bg-cont2">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png"
+        class="image1"
+      />
+      <p class="para-name">Kiran V</p>
+      <p>Vishnu Institute of Computer Education and Technology</p>
+      <p>Bhimavaram</p>
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png"
+        class="image2"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
